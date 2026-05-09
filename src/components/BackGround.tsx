288820ed455dff1e143e8c830b180e0ced28adbf
Da9TH5e <debarkomandal@gmail.com>
2026-05-09 19:56:59 +0530

    refactor: Update BackGround component styles for improved layout and responsiveness

--- src/components/BackGround.tsx
+++ src/components/BackGround.tsx
@@ -1,13 +1,16 @@
 export function BackGround({ className }: {className?: string}) {
   return (
     <div className={`
-        flex
-        overflow-y-auto
-        w-screen
-        h-fill
-        ${ className }
+        bg-[url(/background_img_1.png)]
+        bg-cover
+        bg-center
+        sm:w-325
+        xl:w-full
+        bg-no-repeat
+        w-325
+        h-full
+        ${className}
     `}>
-      <img src="/background_img_1.png" />
     </div>
   )
 }
